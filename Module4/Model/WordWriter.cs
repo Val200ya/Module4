aa27826a04ceb48203ffddd7cb18970e65b08718
Val200ya <australia636@gmail.com>
2025-03-28 01:04:09 +0300

    Тяжело

---
+++ Module4/Model/WordWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Module4.Model
+{
+    public class WordWriter
+    {
+        public void writeToWordTable(string filePath, int tableIndex, int rowIndex, int columnIndex, string data)
+        {
+            Word.Application wordApp = new Word.Application();
+            wordApp.Visible = false;
+
+            try
+            {
+                Word.Document wordDocument = wordApp.Documents.Open(filePath);
+                Word.Table wordTable = wordDocument.Tables[tableIndex];
+
+                if (columnIndex < 1 || columnIndex > wordTable.Columns.Count)
+                {
+                    throw new ArgumentException("Неверный индекс колонки.");
+                }
+                if (rowIndex < 1 || rowIndex > wordTable.Rows.Count)
+                {
+                    throw new ArgumentException("Неверный индекс строки.");
+                }
+
+                wordTable.Cell(rowIndex, columnIndex).Range.Text = data;
+                wordDocument.Save();
+                wordDocument.Close();
+                wordApp.Quit();
+
+                MessageBox.Show("Данные были успешно добавлены в таблицу!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Module4/View/ValidationWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Module4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Module4.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ValidationWindow.xaml
+    /// </summary>
+    public partial class ValidationWindow : Window
+    {
+        ServerRequest request = new ServerRequest();
+        WordWriter writer = new WordWriter();
+        public ValidationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetDataButtonClick(object sender, RoutedEventArgs e)
+        {
+            string url = "http://localhost:4444/TransferSimulator/fullName";
+            string result = await request.getRequestAsync(url);
+            DataTextBlock.Text = getFullName(result);
+        }
+
+        private string getFullName(string result)
+        {
+            return result.Substring(result.IndexOf(":") + 2).Replace("\"", "").Replace("}", "");
+        }
+
+        private bool isContainingUnknownChars(string text)
+        {
+            return Regex.IsMatch(text, @"[^а-яА-ЯёЁ0-9\s]");
+        }
+
+        private void SendResultClickButton(object sender, RoutedEventArgs e)
+        {
+            bool hasUnknownChars = isContainingUnknownChars(DataTextBlock.Text);
+            string result = "";
+            string validationResult = hasUnknownChars ? "ФИО содержит запрещённые символы" : "Ошибок нет";
+            ResultTextBlock.Text = validationResult;
+
+            if (validationResult.Equals("ФИО содержит запрещённые символы"))
+            {
+                result = "Не успешно";
+            }
+            else if (validationResult.Equals("Ошибок нет"))
+            {
+                result = "Успешно";
+            }
+            
+            string filePath = "C:\\Users\\austr\\OneDrive\\Рабочий стол\\ДЕМО\\Module4\\Module4\\ТестКейс.docx";
+            int tableIndex = 1;
+            int columnIndex = 3;
+            int rowIndex = 3;
+
+            writer.writeToWordTable(filePath, tableIndex, rowIndex, columnIndex, result);
+        }
+    }
+}
